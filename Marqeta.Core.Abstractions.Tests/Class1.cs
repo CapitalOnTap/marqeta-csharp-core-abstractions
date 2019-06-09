7662d9a038eb952ca61b2ee1ac8e883534ad4ada
Liam Flanagan <liam.flanagan@capitalontap.com>
2019-06-09 11:59:41 +0100

    Added Xunit and a temporary test class (for testing the pipeline).

---
+++ Marqeta.Core.Abstractions.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
